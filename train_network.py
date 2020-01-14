54b449dfad5cada1e8937ebaf4e6b02ffd1b5e03
Kibrom Haftu <kibromhft@gmail.com>
2020-01-14 18:43:44 +0300

    Create train_network.py

---
+++ train_network.py
@@ -0,0 +1,42 @@
+from keras.layers import Input, Lambda, Dense, Flatten
+from keras.models import Model
+from keras.models import Sequential
+from keras.applications.DenseNet121 import DenseNet121
+from keras.applications.DenseNet121 import preprocess_input
+from keras.preprocessing import image
+import numpy as np
+from glob import glob
+from keras.preprocessing.image import ImageDataGenerator
+#Load the pre-trained model
+#add preprocessing layer to the front of DenseNet
+densenet = DenseNet121(
+    weights='../input/densenet-keras/DenseNet-BC-121-32-no-top.h5',
+    include_top=False,
+    input_shape=(224,224,3)
+)
+
+# No need training for existing weights
+for layer in densenet.layers:
+  layer.trainable = False
+  
+train_dir = './Datasets/Train/train'
+validation_dir = './clean-dataset/validation'
+
+  # This is needed for getting number of classes
+folders = glob('Datasets/Train/*')
+  # our layers - you can add more if you want
+x = Flatten()(densenet.output) ##https://stackoverflow.com/questions/43237124/what-is-the-role-of-flatten-in-keras
+# x = Dense(1000, activation='relu')(x)
+prediction = Dense(len(folders), activation='softmax')(x)
+
+# create a model object
+model = Model(inputs=densenet.input, outputs=prediction)
+# view the structure of the model
+model.summary()
+
+# tell the model what cost and optimization method to use
+model.compile(
+  loss='categorical_crossentropy',
+  optimizer='adam',
+  metrics=['accuracy']
+)
